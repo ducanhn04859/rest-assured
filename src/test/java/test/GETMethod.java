21a710d083d21dfa7dc8c84dae5a330a9c6df246
ducanhn04859 <ducanhn1308@gmail.com>
2022-04-29 17:29:31 +0700

    Rename file

--- src/test/java/test/GETMethod.java
+++ src/test/java/test/GETMethod.java
@@ -5,7 +5,7 @@ import io.restassured.specification.RequestSpecification;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.equalTo;
 
-public class SimpleTest {
+public class GETMethod {
 
     public static void main(String[] args) {
         String baseUrl ="https://jsonplaceholder.typicode.com";
